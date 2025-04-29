89acab83d0e57dd4f23d049a5ad3efff2e598e29
Your Name <vemasani.pavani@gmail.com>
2025-04-29 12:51:56 +0530

    to redirect output and error in one file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # eks-argocd-helm-chart
+
+Output Redirector
+==================
+Output: 1>
+Error: 2>
+
+ex:
+1>/tmp/expense.log
+2>/tmp/expense.log
+
+to redirect both error and ouput we can use &>
+
+ex: &>/tmp/expense.log
+in the above example &> override the previous and display latest , but here we required all output from beginning to the
+end
+so for this we have to use &>>
+
+ex: &>>/tmp/expense.log , displays output from beginning to the end.
+
--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,13 @@
-dnf install nginx -y &>/tmp/expense.log
-systemctl enable nginx &>/tmp/expense.log
-systemctl start nginx &>/tmp/expense.log
-cp expense.conf /etc/nginx/default.d/expense.conf 1>/tmp/expense.log
-rm -rf /usr/share/nginx/html/* 1>/tmp/expense.log
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip 1>/tmp/expense.log
-cd /usr/share/nginx/html 1>/tmp/expense.log
-unzip /tmp/frontend.zip 1>/tmp/expense.log
-systemctl restart nginx 1>/tmp/expense.log
+dnf install nginx -y &>>/tmp/expense.log
+systemctl enable nginx &>>/tmp/expense.log
+systemctl start nginx &>>/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
+cd /usr/share/nginx/html &>>/tmp/expense.log
+unzip /tmp/frontend.zip &>>/tmp/expense.log
+systemctl restart nginx &>>/tmp/expense.log
+
+
+
+
