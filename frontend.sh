68e541f04082a7429531492b321d7743845e8b80
Your Name <vemasani.pavani@gmail.com>
2025-04-29 12:43:51 +0530

    redirect the output and error

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 dnf install nginx -y &>/tmp/expense.log
-systemctl enable nginx >/tmp/expense.log
-systemctl start nginx >/tmp/expense.log
+systemctl enable nginx &>/tmp/expense.log
+systemctl start nginx &>/tmp/expense.log
 cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
