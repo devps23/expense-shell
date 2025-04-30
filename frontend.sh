c49a18447e6c84f0f966b218e9d5201f08a223dd
Your Name <vemasani.pavani@gmail.com>
2025-04-30 21:43:40 +0530

    DRY

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,14 @@ print_task_heading "Install nginx"
 dnf install nginx -y &>>Log
 checkStatus $?
 
+print_task_heading "Enable Nginx"
+systemctl enable nginx &>>Log
+checkStatus $?
+
+print_task_heading "Start nginx"
+systemctl start nginx &>>Log
+checkStatus $?
+
 print_task_heading "Copy reverse proxy configuration"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>Log
 checkStatus $?
@@ -16,13 +24,7 @@ checkStatus $?
 
 app_code
 
-print_task_heading "Enable Nginx"
-systemctl enable nginx &>>Log
-checkStatus $?
 
-print_task_heading "Start nginx"
-systemctl start nginx &>>Log
-checkStatus $?
 
 print_task_heading "Restart nginx"
 systemctl restart nginx &>>Log
