51dd5e6666426aa99d25a5a771733eba987a4354
Your Name <vemasani.pavani@gmail.com>
2025-04-30 15:46:48 +0530

    heading redirect into a file

--- backend.sh
+++ backend.sh
@@ -7,6 +7,7 @@ fi
 
 print_task_heading(){
   echo $1
+  echo "**************** $1 ******************" &>>/tmp/expense.log
 }
 print_task_heading "Disable default nodejs"
 dnf module disable nodejs -y &>>/tmp/expense.log
